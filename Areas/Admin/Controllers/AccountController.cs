6e3012fbecbff9e5b86abb5ea92dc5ae921f5f99
Huu Tam <Christopherle0902@gmail.com>
2024-09-21 16:41:43 +0700

    up

---
+++ Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using LeHuuTam.Data;
+using LeHuuTam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeHuuTam.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AccountController(ILogger<AccountController> logger, ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _dbContext.Users.ToList();
+            return View(result);
+        }
+      
+
+    }
+}
