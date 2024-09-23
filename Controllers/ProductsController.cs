88ce303ce6a7897603c2b73d642c0bdb8b1026be
Huu Tam <Christopherle0902@gmail.com>
2024-09-23 16:53:38 +0700

    update

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using LeHuuTam.Areas.Admin.Controllers;
+using LeHuuTam.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace LeHuuTam.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<ProductsController> _logger;
+
+        // Constructor
+        public ProductsController(ILogger<ProductsController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        // Action to get the product list
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var result = _dbContext.Products.ToList();
+            return View(result);
+        }
+    }
+}
